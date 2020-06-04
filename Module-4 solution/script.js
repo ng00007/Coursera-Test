df293b4a64c5d4ac26420010c65afbc2b176cda6
ng00007 <64907627+ng00007@users.noreply.github.com>
2020-06-04 21:06:52 +0530

    Update script.js

--- Module-4 solution/script.js
+++ Module-4 solution/script.js
@@ -1,6 +1,6 @@
 
 (function() {
-    var names = ["Yogesh", "John", "Jen", "Jason", "Paul", "Frank", "Terry", "Ronaldo", "Shane", "Sherlock"];
+   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
